1c9629e9980ef446d40288b6afd4bca8214231e6
XUHO3 <howells.xu@oocl.com>
2018-07-03 19:47:13 +0800

    session one -four

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,26 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+ const result =new Array();
+     // var collA=new Array();
+   for(var n=0;n<collectionA.length;n++){
+    var objectA=collectionA[n];
+     for(var name in objectA){
+       var collA=objectA[name];
+       console.log(collA);
+       for(var name in objectB){
+         var collB=objectB[name];}
+         console.log(collB);
+         console.log(collB);
+         for(var j=0;j<collB.length;j++){
+              if(collA==collB[j]){
+                console.log(collA);
+                result.push(collA);
+                break;
+              }
+         }
+     }
+   }
+    console.log(result);
+     return result;
 }
