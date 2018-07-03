f1d8f30024ba73c95b20f28c800a19ffb946f643
WenhaoXu <1787107003@qq.com>
2018-07-03 23:02:11 +0800

    session three -three

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,44 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var key1 = new Array();
+  var value1 = new Array();
+  var json = [];
+  for (var i = 0; i < collectionA.length; i++) {
+    if (key1.length == 0) {
+      key1.push(collectionA[i]);
+      value1.push(1);
+      continue;
+    }
+    else {
+      for (var j = 0; j < key1.length; j++) {
+        if (key1[j] == collectionA[i]) {
+          value1[j]++;
+          console.log(key1[j]);
+          console.log(value1[j]);
+          break;
+        }
+        if (j == key1.length - 1 && key1[j] != collectionA[i]) {
+          key1.push(collectionA[i]);
+          value1.push(0);
+        }
+      }
+
+    }
+  }
+  for (var i = 0; i < key1.length; i++) {
+    var word = {key: key1[i], count: value1[i]};
+    console.log(word)
+    json.push(word);
+  }
+  var list=objectB.value;
+  for(var i=0;i<list.length;i++){
+    var word=list[i];
+    for(var j=0;j<json.length;j++){
+      if(word==json[j].key){
+        json[j].count=json[j].count-parseInt(json[j].count/3);
+      }
+    }
+  }
+  return json;
 }
