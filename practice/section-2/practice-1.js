8d87777c529b8a978060bb2cf8b5ca6fee95ed94
XUHO3 <howells.xu@oocl.com>
2018-07-03 21:16:05 +0800

    session two -one

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var key1 = new Array();
+  var value1 = new Array();
+  var json = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (key1.length == 0) {
+      key1.push(collection[i]);
+      value1.push(1);
+        continue;
+    }
+    else {
+      for (var j = 0; j < key1.length; j++) {
+        if (key1[j] == collection[i]) {
+          value1[j]++;
+          console.log(key1[j]);
+          console.log(value1[j]);
+          break;
+        }
+        if (j == key1.length - 1 && key1[j] != collection[i]) {
+          key1.push(collection[i]);
+          value1.push(0);
+        }
+      }
+
+    }
+  }
+  for (var i = 0; i < key1.length; i++) {
+    var word = {key: key1[i], count: value1[i]};
+    console.log(word)
+    json.push(word);
+  }
+  return json;
 }
